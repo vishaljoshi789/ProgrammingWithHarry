7ccae62182bea2ed160d01f4a8875387628cb636
Vishal <visjoshi1234@gmail.com>
2021-06-04 16:36:49 +0530

    added more responsiveness to navbar

--- js/app.js
+++ js/app.js
@@ -1,9 +1,12 @@
 collapsebtnclicked = () => {
     let nav = document.getElementsByTagName("nav")[0];
-    if (nav.id === "collapseoff") {
-        document.getElementById("collapseoff").id = "collapseon";
+    if (nav.id === "collapsef") {
+        document.getElementById("collapsef").id = "collapseon";
     } else if (nav.id === "collapseon") {
         document.getElementById("collapseon").id = "collapseoff";
+
+    } else if (nav.id === "collapseoff") {
+        document.getElementById("collapseoff").id = "collapseon";
     }
 
 
