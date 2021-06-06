82228720a02cf4e97a70414c9d73cfd8079ca1b6
Vishal <visjoshi1234@gmail.com>
2021-06-06 13:05:09 +0530

    completed video fetching from youtube

---
+++ js/videolist.js
@@ -0,0 +1,123 @@
+let vlist = {
+    // python videos
+    "python absolute beginner": {
+
+        "Installing VS Code, Python & writing hello world Python code | Python Tutorial #1": { 'thumb': 'https://i.ytimg.com/vi/ks6wF_ooYqo/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLBUPO4YRXI4evALpqiAaqCHskch9Q', "videoid": "ks6wF_ooYqo" },
+        "Variables & Comments in Python | Python Tutorial #2": { 'thumb': 'https://i.ytimg.com/vi/lOHF1WfpDaY/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLDfsCGOPtOkFRZ3Ei8qLaVLeZdwCQ', "videoid": "lOHF1WfpDaY" },
+        "Numbers & Typecasting in python | Python Tutorial #3": { 'thumb': 'https://i.ytimg.com/vi/a5xyuIw96ww/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLCZNxiGimzKfKUcYgnSfbCaYL_K6Q', "videoid": "a5xyuIw96ww" },
+        "Operators in Python | Python Tutorial #4": { 'thumb': 'https://i.ytimg.com/vi/maIAaMbf7ag/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLD5O723Xkw0gI-X4s2z-cAsemE7CA', "videoid": "maIAaMbf7ag" },
+        "Python strings and built in string methods | Python Tutorial #5": { 'thumb': 'https://i.ytimg.com/vi/UcgCOpXTDjs/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLCYLYzehLZGBloPvqeSaF22cSxI1w', "videoid": "UcgCOpXTDjs" },
+        "Exercise 1: Capitalize my name & String concatenation | Python Tutorial #6 ": { 'thumb': 'https://i.ytimg.com/vi/thZregq8x9I/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLDIKqXWD3v6dj6gr7Io3JpUXgvtiA', "videoid": "thZregq8x9I" },
+        "F-strings in Python | Python Tutorial #7": { 'thumb': 'https://i.ytimg.com/vi/cKNNr1kqj-k/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLBg9hmPOAA2rAHAvXhNOR6hrvDirw', "videoid": "cKNNr1kqj-k" },
+        "Taking user input in Python  | Python Tutorial #8 ": { 'thumb': 'https://i.ytimg.com/vi/qyXbDxMHhs4/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLB_OzaFWNnJSQfPK9nzq6BOSpNXQg', "videoid": "qyXbDxMHhs4" },
+        "Lists in Python | Python Tutorial #9 ": { 'thumb': 'https://i.ytimg.com/vi/_oWVDNRkRKs/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLBnpe3vL7gy7Hb1Mw61eFMNrEq4Aw', "videoid": "_oWVDNRkRKs" },
+        "Tuple in Python | Python Tutorial #10 ": { 'thumb': 'https://i.ytimg.com/vi/aVHNlC-cAjw/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLB2awnYwI5cvSpPJdviia9CHh5MBg', "videoid": "aVHNlC-cAjw" },
+        "Dictionary and related methods in Python | Python Tutorial #11 ": { 'thumb': 'https://i.ytimg.com/vi/tT7iiYkeCPk/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLAqeftRplQHWaMzMXeTNhh4JAaQ3w', "videoid": "tT7iiYkeCPk" },
+        "Sets in Python | Python Tutorial #12 ": { 'thumb': 'https://i.ytimg.com/vi/Avg_Gjsm7BI/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLCmaBPY74l2hNpyxGRh8xznSV2z4g', "videoid": "Avg_Gjsm7BI" },
+        "If else conditionals in Python | Python Tutorial #13 ": { 'thumb': 'https://i.ytimg.com/vi/gQYMlDdahMY/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLAe2yguJCnN3CkuDFxiqxFXRdfoKg', "videoid": "gQYMlDdahMY" },
+        "For Loops in python | Python Tutorial #14 ": { 'thumb': 'https://i.ytimg.com/vi/4A6FCmUfjyY/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLCykoxtPsdvQqMMu6hoGUSHKXgDiw', "videoid": "4A6FCmUfjyY" },
+        "While Loops in python | Python Tutorial #15": { 'thumb': 'https://i.ytimg.com/vi/8Baiw_bwTwI/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLC3p-yZ4MocJby4SmuCWi-Vv8fv-g', "videoid": "8Baiw_bwTwI" },
+        "Exercise 1: Solution + Shoutouts | Python Tutorial #16 ": { 'thumb': 'https://i.ytimg.com/vi/0YMl1MqMHOw/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLDwtLe1018Qex5Zvj5s3uin4Ikyyw', "videoid": "0YMl1MqMHOw" },
+        "Functions in Python | Python Tutorial #17 ": { 'thumb': 'https://i.ytimg.com/vi/PfvQjwYg798/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLAOWSbC-GnRzJQb-ymx7Cx5-Ppf3g', "videoid": "PfvQjwYg798" },
+        "Exercise 2: Twinkle Twinkle Little Star | Python Tutorial #18": { 'thumb': 'https://i.ytimg.com/vi/ZZmCcASvYAs/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLDeUXF0ZjEHC-stPXdQPLS8_1CpMQ', "videoid": "ZZmCcASvYAs" },
+        "Recursions in Python | Python Tutorial #19": { 'thumb': 'https://i.ytimg.com/vi/QPvkogmM3jA/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLCj6_yzB5Y-qWBIizM8f7OHnD3C6Q', "videoid": "QPvkogmM3jA" },
+        "Modules & pip in Python | Python Tutorial #20": { 'thumb': 'https://i.ytimg.com/vi/8mM-oY2OD0k/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLC9vnkqk4B_CvqGFHXt8eMzX4KZww', "videoid": "8mM-oY2OD0k" },
+        "For else in Python | Python Tutorial #21": { 'thumb': 'https://i.ytimg.com/vi/GRLKc5YdqMg/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLCop9WqVXXyjIR2kcxHEQjowYmffg', "videoid": "GRLKc5YdqMg" },
+        "Try except and Error handling in Python | Python Tutorial #22": { 'thumb': 'https://i.ytimg.com/vi/P--ajCoYFiM/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLDAIUE8MLw_1c3W76z32z2P5wY0ZA', "videoid": "P--ajCoYFiM" },
+        "Exercise 2: Solution + Shoutouts | Python Tutorial #23": { 'thumb': 'https://i.ytimg.com/vi/wNXjVjRT8Ec/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLAZ3N7zM9gb-hI7QiCo-qNE5RjoyA', "videoid": "wNXjVjRT8Ec" },
+        "Short Hand If - Else Notation in Python | Python Tutorial #24": { 'thumb': 'https://i.ytimg.com/vi/4vsW0LimYh8/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLBVtzojFOhIwv4gnUIcuqJMP9h1IA', "videoid": "4vsW0LimYh8" },
+        "Escape sequence characters in Python | Python Tutorial #25": { 'thumb': 'https://i.ytimg.com/vi/7g6mQnpOxgE/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLCir41rtD10nyuFS7hvAIWEjAYgdA', "videoid": "7g6mQnpOxgE" },
+        "File Handling in Python | Python Tutorial #26": { 'thumb': 'https://i.ytimg.com/vi/63oykCokNak/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLDHxnrjkSYUpKg_aH2bTii3Yv3_lQ', "videoid": "63oykCokNak" },
+        "f.read(), f.readline() & f.readlines() in Python | Python Tutorial #27": { 'thumb': 'https://i.ytimg.com/vi/jBR5HnKfqCU/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLAAD9fbuIvWg96LHqQFjBah65zsQQ', "videoid": "jBR5HnKfqCU" },
+        "Guess the number: Python Exercise 3 | Python Tutorial #28": { 'thumb': 'https://i.ytimg.com/vi/x4Rt-6lg1d0/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLAAVKmP9dtvoFpKsX0bLJtLFj_W8A', "videoid": "x4Rt-6lg1d0" },
+        "Number Guessing Game: Python Exercise 3 Solution | Python Tutorial #29": { 'thumb': 'https://i.ytimg.com/vi/TXDWEwffQNw/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLDCduPKuqXBoGcMCwPWlPsHvzkWBw', "videoid": "TXDWEwffQNw" },
+        "List Comprehensions In Python | Python Tutorial #30": { 'thumb': 'https://i.ytimg.com/vi/ADcuPqLLFWk/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLBezeH-SIv-0L_iC2JLC6-jABYSaw', "videoid": "ADcuPqLLFWk" },
+        "Iterators, Iterables & Generators In Python | Python Tutorial #31": { 'thumb': 'https://i.ytimg.com/vi/YFsN4Y0Q7FI/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLCdP7ti5nTM2mT3s0jVs19bYGSVfg', "videoid": "YFsN4Y0Q7FI" },
+        "Lambda Functions In Python | Python Tutorial #32": { 'thumb': 'https://i.ytimg.com/vi/wwXFXuRETdY/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLAgdiIm8adRVvIVK_S0MSTfu2fI0g', "videoid": "wwXFXuRETdY" },
+        "Project 1: Jarvis - Python AI Virtual Desktop Assistant | Python Tutorial #33": { 'thumb': 'https://i.ytimg.com/vi/4k9CphTdnWE/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLDxZ4fOqEN9yUnIM4Sq6PB2P8LcQQ', "videoid": "4k9CphTdnWE" },
+        "Object Oriented Programming - Classes & Objects in Python | Python Tutorial #34": { 'thumb': 'https://i.ytimg.com/vi/m53EEP5H6Sg/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLCcRR2YqZ7c2Vxhuyx1nAYWfBVuNA', "videoid": "m53EEP5H6Sg" }
+    },
+
+    // django videos
+    "django tutorial for absolute beginner": {
+        "Installing VS Code, Django & More | Django Tutorial #1": { 'thumb': 'https://i.ytimg.com/vi/MxF8YOJI0nU/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLC4cktYAFmYXtb8Qj-qVkLe2OEM8A', "videoid": "MxF8YOJI0nU" },
+        "How Django Works with HTML, CSS & JavaScript | Django Tutorial #2": { 'thumb': 'https://i.ytimg.com/vi/pHQj9dVw5OA/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLAw-jT3DQGOdVoBkKQKuGi9VXUe2w', "videoid": "pHQj9dVw5OA" },
+        "Folder Structure of a Django Application | Django Tutorial #3": { 'thumb': 'https://i.ytimg.com/vi/ghyntulVyqA/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLCQegkpNi8NxXn9cu8Md-dNlrjI6w', "videoid": "ghyntulVyqA" },
+        "Apps, Projects and UrlPatterns in Django | Django Tutorial #4": { 'thumb': 'https://i.ytimg.com/vi/Ay4P500hZdE/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLCE81gb7Vsha-8p4owClvjC6hgWrQ', "videoid": "Ay4P500hZdE" },
+        "MVT Pattern in Django | Django Tutorial #5": { 'thumb': 'https://i.ytimg.com/vi/eBBmnbYontM/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLALsFh8i6ang0ajAfYiCm_NO2dgaQ', "videoid": "eBBmnbYontM" },
+        "Creating a Developer Portfolio Website Using Django | Django Tutorial #6": { 'thumb': 'https://i.ytimg.com/vi/46QEz7g9esc/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLDFneFr0QGrf5alHlMWy5x2E9LQ8g', "videoid": "46QEz7g9esc" },
+        "Using Templates & Django Template Engine in Django | Django Tutorial #7": { 'thumb': 'https://i.ytimg.com/vi/LQLg-Gvioes/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLASOsrmIHfJhDFStNgzPP7J43XzMg', "videoid": "LQLg-Gvioes" },
+        "Portfolio Website: Integrating Bootstrap with Django | Django Tutorial #8": { 'thumb': 'https://i.ytimg.com/vi/nsF0NLwkSPA/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLDClATggD2LA0FR4MWmhIbThtYQcQ', "videoid": "nsF0NLwkSPA" },
+        "Adding Further Design to our Developer Portfolio Website | Django Tutorial #9": { 'thumb': 'https://i.ytimg.com/vi/ESky4QTOVZo/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLCh-PI2iT2eu4E7pIq6l_iJBU9cTQ', "videoid": "ESky4QTOVZo" },
+        "Models in Django: Saving Form Data To The Database | Django Tutorial #10": { 'thumb': 'https://i.ytimg.com/vi/To879URvX0c/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLC_9pgH06YVp9uE6aqSsj1aqhcYiA', "videoid": "To879URvX0c" },
+        "Understanding the Django Admin Interface | Django Tutorial #11": { 'thumb': 'https://i.ytimg.com/vi/iC7rc7LQFRc/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLAVh-2neGE21xVHWD7ij1s31JOsmQ', "videoid": "iC7rc7LQFRc" },
+        "Customizing the Django Admin Interface | Django Tutorial #12": { 'thumb': 'https://i.ytimg.com/vi/nIMYRqwFpVM/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLBF8Srxy7ogq-t5zDdFTZBbd1bs8w', "videoid": "nIMYRqwFpVM" },
+        "Using Static Files & StaticFiles Dirs in Django | Django Tutorial #13": { 'thumb': 'https://i.ytimg.com/vi/G-kdYhufgDo/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLB0oombgS-JTmZY00rIV-cigapkPw', "videoid": "G-kdYhufgDo" },
+        "Template Inheritance in Django | Django Tutorial #14": { 'thumb': 'https://i.ytimg.com/vi/gw-hKVt71A8/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLBM0ZN4dQsThfrdzbG85LqBCxl9zw', "videoid": "m53EEP5H6Sg" },
+        "Project 2: ToDos List App in Django | Django Tutorial #15": { 'thumb': 'https://i.ytimg.com/vi/bMNdcMSe3pc/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLB_70_aHbZclybbawXkkGixiyFJxw', "videoid": "gw-hKVt71A8" },
+        "Project 2: Creating Models & Superuser for Our ToDos List Django App | Django Tutorial #16": { 'thumb': 'https://i.ytimg.com/vi/CuKsIVwZPXw/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLCOXd_KHCIGDK-Jvoc6McoeGyEOBQ', "videoid": "CuKsIVwZPXw" },
+        "Project 2: Using Django Template Language in our ToDos List Django App | Django Tutorial #17": { 'thumb': 'https://i.ytimg.com/vi/DR0Kbx1N1TU/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLCds7o_vCUQUG-Ip0kJqAON3bG5cA', "videoid": "DR0Kbx1N1TU" },
+        "Project 2: Fetching Data from our Models for ToDos List Django App | Django Tutorial #18": { 'thumb': 'https://i.ytimg.com/vi/KaJBkELJ5n0/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLD_euINWEewnZYhDh_6AK_paKZJEw', "videoid": "KaJBkELJ5n0" },
+        "Project 2: Finishing Up ToDos List Django App | Django Tutorial #19": { 'thumb': 'https://i.ytimg.com/vi/GkJgDfI17ec/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLBGCqKrLWhhFTf8_NYithw3dN_l2A', "videoid": "GkJgDfI17ec" },
+        "Django Authentication and Authorization Tutorial | Django Tutorial #20": { 'thumb': 'https://i.ytimg.com/vi/79RPkGS7IPs/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLBftEDW_UfCZowKkdMfGNX7CjudPA', "videoid": "79RPkGS7IPs" },
+        "Django CMS: Creating a Django Blog | Django Tutorial #21": { 'thumb': 'https://i.ytimg.com/vi/ig532b4WkQc/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLB-e9ORzXdQWJof908OR4DeCS3UnA', "videoid": "ig532b4WkQc" },
+        "Django CMS: Adding Bootstrap to our Django Blog | Django Tutorial #22": { 'thumb': 'https://i.ytimg.com/vi/ZzkeHroO56c/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLBPAC49S2-LAp_JZL3-LTLy7hexAg', "videoid": "ZzkeHroO56c" },
+        "Django Blog: Creating Blog Model For our Mountain Coders Blog | Django Tutorial #23": { 'thumb': 'https://i.ytimg.com/vi/wN9ukxl0Z6A/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLApiifv7s4zJh-LAWhw1jMq_FYQsw', "videoid": "wN9ukxl0Z6A" },
+        "Django Blog: Designing the Blog Home Page | Django Tutorial #24": { 'thumb': 'https://i.ytimg.com/vi/-C3s93LgpJ8/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLAlv58EaloYKDgFgYqsIniXEZUiSw', "videoid": "-C3s93LgpJ8" },
+        "Django Blog: Rendering the Blog on Blog Page | Django Tutorial #25": { 'thumb': 'https://i.ytimg.com/vi/dS3loltS_mY/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLAUeAa9OZDNYEzYufYZi8DW-ZSChQ', "videoid": "dS3loltS_mY" },
+        "Django Blog: Designing The HomePage | Django Tutorial #26": { 'thumb': 'https://i.ytimg.com/vi/_CZ3J7J36kk/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLA0cjNYPz3Ufy5BpM6e0zITWHm1Qw', "videoid": "_CZ3J7J36kk" },
+        "Adding Rich Text Editor to the Django Admin | Django Tutorial #27": { 'thumb': 'https://i.ytimg.com/vi/IdCs-an5QLA/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLBd1Jo7DP6ugSlXCp5KO5e8oAkmjw', "videoid": "IdCs-an5QLA" },
+        "Finishing up Tiny MCE Rich Text Editor | Django Tutorial #28": { 'thumb': 'https://i.ytimg.com/vi/kkfXDu8jc9I/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLBOiVTW5KKbniL8Qax0axHe3kQOJw', "videoid": "kkfXDu8jc9I" },
+        "Customizing Tiny MCE For Better Editing Experience | Django Tutorial #29": { 'thumb': 'https://i.ytimg.com/vi/JO58rTZI3ak/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLB3SbklpTBQzFUUT7F1Hp3jdW0B8A', "videoid": "JO58rTZI3ak" },
+        "Making Design Improvements in Our Django Blog | Django Tutorial #30": { 'thumb': 'https://i.ytimg.com/vi/al0tAGjpMO4/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLAN4KoO6NhTC3LZteNR69ftxoVrQQ', "videoid": "al0tAGjpMO4" },
+        "Designing The Pagination Logic For Our Django Blog | Django Tutorial #31": { 'thumb': 'https://i.ytimg.com/vi/yvyFzj0C60Y/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLCbxYJFiSgfacJGMNW85fmvHWqNLw', "videoid": "yvyFzj0C60Y" },
+        "Creating Contact Model + UI for our Django Blog | Django Tutorial #32": { 'thumb': 'https://i.ytimg.com/vi/VdYnH9z6CeY/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLCcopCYb6v15V4-MXjD1MVWh2gBpg', "videoid": "VdYnH9z6CeY" },
+        "Adding Virtual Environment to our Django Blog | Django Tutorial #33": { 'thumb': 'https://i.ytimg.com/vi/7Z_ZTsop7qQ/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLACOG2eJ-XgIGWuyh_7E97JXbzDzg', "videoid": "7Z_ZTsop7qQ" },
+        "Deploying our Django Blog in Ubuntu 18.04 VPS | Django Tutorial #34": { 'thumb': 'https://i.ytimg.com/vi/zg1PacxR4r8/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLB28KFwzWJdhnKNKFdB3GvolUYJBQ', "videoid": "zg1PacxR4r8" }
+
+
+    },
+    // learn in one videos
+    "learnInOne": {
+        "Python Tutorial For Beginners (Complete Course)": { 'thumb': 'https://i.ytimg.com/vi/IMGzzK9Wn4w/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCCLbpd8zp4VIeJRj1IkokZ7PmfBg', "videoid": "IMGzzK9Wn4w" },
+        "HTML for Web Development Tutorial for Beginners (2020 Updated Course)": { 'thumb': 'https://i.ytimg.com/vi/lsxt002Zups/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAWQ3uLvoU1QfpspQzCrSvT-kV87A', "videoid": "lsxt002Zups" },
+        "Django 2.2 Tutorial For Beginners": { 'thumb': 'https://i.ytimg.com/vi/OSBOZdwMCNw/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCS9BGHigCFo4guas6KJAOEe4MH4A', "videoid": "OSBOZdwMCNw" }
+    }
+
+
+}
+
+getvideofun = (videoid, key) => {
+    localStorage.removeItem("videoid")
+    localStorage.removeItem("videotitle")
+    localStorage.setItem("videoid", videoid)
+    localStorage.setItem("videotitle", key)
+}
+categoryfun = (category) => {
+    localStorage.clear()
+    if (category === 'python') {
+
+        localStorage.setItem('Python Tutorials For Beginners In English', JSON.stringify(vlist['python absolute beginner']))
+    } else if (category === 'python-django') {
+        localStorage.setItem('Python Django Tutorials In English', JSON.stringify(vlist["django tutorial for absolute beginner"]))
+
+    } else if (category === 'learnInOne') {
+        localStorage.setItem('Learn in one video', JSON.stringify(vlist["learnInOne"]))
+
+    }
+
+}
+
+videos = JSON.parse(localStorage.getItem(localStorage.key(0)))
+let videolist = document.getElementById("video-list");
+for (video in videos) {
+    videolist.innerHTML += `<div class="card d-flex">
+    <div>
+    <img class="card-img" style="width:100%;height:100%;border-radius: 0px;" src="${videos[video]['thumb']}" alt="">
+    </div>
+    <div class="card-heading" style="margin:10px;">
+    <h4>${video}</h4>
+    </div>
+    <a  href="video.html"id="watchbtn" ><button
+        class="card-button" onclick="getvideofun('${(videos[video])['videoid']}','${video}')">Watch</button></a>`
+}
