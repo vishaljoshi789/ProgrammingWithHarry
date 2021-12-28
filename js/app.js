ebbc157c67b561d2321f5b31fd6ac69414c633ae
Vishal Joshi <vishalwebdev0987@gmail.com>
2021-12-28 15:09:16 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -10,20 +10,4 @@ collapsebtnclicked = () => {
         document.getElementById("collapseoff").id = "collapseon";
     }
 
-
 }
-
-// hello harry bhai this is vishal joshi the one ho made your frontend of  programmingwithharry 
-// I am a student of class 12th and I am also passonate in coding same as you,
-// I am of middle class family got a tons of loan on them, in order to help my parents by any way
-// I started doing freelancing. I was on class 9th when 
-// I first watched your python videos by that videos my interest for cs was increasing and learned a lot about programming 
-// languages your videos helped me a lot then I got little intrest on web development site because that do not require 
-// powerful hardware unlike android development and all. I learned HTML CSS by your videos which you have uploded a few 
-// time ago and then learned javascript("ofcourse your javascript in one video") and  then i thought i should start doing
-// freelancing but as you said 'pehela kaam milna muskil hota he ', but i dont know how but i found you and you have 
-// give me my first order, now as a teacher you helped me a lot by teaching me and give me a chance to grow,
-//  but I am unwilling to say that i was unable to make online indicator for your online presence
-// I am really very sorry for that but I have got  logic for that, just after delevering you your project
-// I am going to start learing backend and I will make your  online indicator as you asked for I am really very sorry for that
-//  hope the frontend work was good
